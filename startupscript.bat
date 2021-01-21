0933df0d6a11c878b6360cf5ddc84c03ef91f70e
George Poulos <geomanagementgr@gmail.com>
2021-01-21 19:03:08 +0200

    Create startupscript.bat

---
+++ startupscript.bat
@@ -0,0 +1,4 @@
+
+:: start upfolder C:\Users\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+"C:\Users\Documents\visualstudiotests\Develop\env\scripts\activate"
+python "C:\Users\Documents\visualstudiotests\Develop\manage.py" twitter_util -a
